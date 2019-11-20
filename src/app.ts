c4b5fd21125f65ff776ed52706b0cc501dfbcb51
Linus Jansson <00Jansson@gmail.com>
2019-11-20 15:38:20 +0100

    Login progress, added session and cookeparser

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
-
+// Set up env variables
 import * as dotenv from "dotenv";
 dotenv.config();
 
+// Other imports
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import * as express from "express";
+import session from "express-session";
+import passport from "passport";
 import setUpHBS from "./modules/handlebarsSetup";
 import logger from "./modules/logger";
 import router from "./routes/Default";
@@ -11,6 +17,12 @@ import app from "./Server";
 setUpHBS(app);
 app.use(router);
 
+// Setup cookieparser, bodyparser, express session, and passport
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({ secret: process.env.COOKIESECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
 // Start the server
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
